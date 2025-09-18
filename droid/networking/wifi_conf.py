3173e0a25d9a396645c41e27ae7d822eb8704faf
usaGi <usag1@protonmail.com>
2025-09-18 01:50:42 +0100

    Initial commit for pi network configuration script

---
+++ droid/networking/wifi_conf.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# // Script for wifi configuration on pi via ssh, or anything on ssh for that matter
+#    Ensure that you have wireless_tools installed (arch) or at least iwlist
+
+# // Imports
+import os
+import subprocess
+import re
+import sys
+
+# Functionality
+
+# Get Network Interfaces: 
+
+def get_network_interfaces():
+    """Function that gets network interface from user and returns it"""
+    print("// Interfaces:")
+    os.system("ip route list")
+    interface = input("// Please input network interface: ")
+    return interface
+# Get available networks: 
+
+def get_networks(interface):
+    """Function that returns the available WiFi networks """
+    print("// WiFi Network Scan Commencing...")
+    # Start sub-process that attempts to scan networks 
+    try:
+        scan_results = subprocess.check_output(["sudo", "iwlist", interface, "scan"], universal_newlines=True)
+        # Extract the SSIDs from the scan results: 
+        available_networks = re.findall(r'ESSID:"([^"]*)', scan_results)
+
+        # Perform check to ensure unique networks only are returned
+        unique_networks = []
+        for network in available_networks:
+            if network and network not in unique_networks:
+                unique_networks.append(network)
+        return unique_networks
+
+    except Exception as e:
+        print(f"// Error during network scan: {e}")
+
+# Select Network:
+def select_network(networks):
+    if not networks: 
+        print("// No Networks found...")
+        return None
+    print("\n// Available Networks:\n")
+    for i, network in enumerate(networks, 1):
+        print(f"    {i}: {network}")
+
+    while True:
+        try:
+            choice = int(input("\n Select Network:"))
+            if 1 <= choice <= len(networks):
+                return networks[choice -1]
+            else: 
+                print("// Invalid Selection..Try Again:")
+        except ValueError as e:
+            raise e
+
+# WiFi network configuration
+
+def configure_network(ssid, password=None):
+    # Configure the device to connect to a specific network:
+    if not password:
+        password = input("// Input Network Password: \n")
+
+    # Need to provide the path to wpa_supplicant.conf - this is where we store our chosen network details
+    wpa_supplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
+    #first_run = input("// Do you want to create a new wpa_supplicant.conf file? Y/n: ")
+    # Make sure that we have a backup of our previous conf - dev idea - we can save these to connect to known networks @ a later time
+
+    #if first_run == "Y":
+     #   create_config_cmd = f"sudo touch {wpa_supplicant_path}"
+
+    print("// Creating backup config file")
+    backup_cmd = f"sudo cp {wpa_supplicant_path} {wpa_supplicant_path}.bak"
+    # Then execute the command using the os lib: 
+    os.system(backup_cmd)
+        
+    # Creating the configuration template: - Need to write something that checks if this file is present in the first place
+    network_config = f'''ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev update_config=1
+country=UK
+network={{
+    ssid="{ssid}"
+    psk="{password}"
+    key_mgmt=WPA-PSK
+}}
+'''
+    print(network_config)
+    # We then want to write this configuration to a temp file: 
+    with open('/etc/wpa_supplicant/wpa_supplicant.conf', 'w') as f:
+        f.write(network_config)
+
+# Restart networking
+    choice = input("// Do you want to restart WiFi & reconnect? Y/n: ")
+    if choice == "Y":
+        os.system("sudo systemctl restart wpa_supplicant")
+        os.system("sudo wpa_cli -i wlan0 reconfigure")
+    else:
+        print("// Config Updated...")
+
+if __name__ == "__main__":
+    interface = get_network_interfaces()
+    x = get_networks(interface)
+    y = select_network(x)
+    configure_network(y)
+
